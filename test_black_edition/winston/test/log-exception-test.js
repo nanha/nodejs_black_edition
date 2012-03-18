f17e0b8a1198ce47d56c9892d02ff4cd94945f44
root <nanhap@gmail.com>
2012-03-18 16:29:51 +0900

    [feature] tdd winston. async

---
+++ test_black_edition/winston/test/log-exception-test.js
@@ -0,0 +1,35 @@
+/*
+ * exception-test.js: Tests for exception data gathering in winston.
+ *
+ * (C) 2010 Charlie Robbins
+ * MIT LICENSE
+ *
+ */
+
+var assert = require('assert'),
+    path = require('path'),
+    spawn = require('child_process').spawn,
+    vows = require('vows'),
+    winston = _winston,
+    helpers = require('./helpers');
+
+vows.describe('winston/logger/exceptions').addBatch({
+  "When using winston": {
+    "the unhandleException() method": {
+      topic: function () {
+        var that = this,
+            child = spawn('node', [path.join(__dirname, 'fixtures', 'scripts', 'unhandle-exceptions.js')]),
+            exception = path.join(__dirname, 'fixtures', 'logs', 'unhandle-exception.log');
+        
+        helpers.tryUnlink(exception);
+        child.on('exit', function () {
+          path.exists(exception, that.callback.bind(this, null));
+        });
+      },
+      "should not write to the specified error file": function (err, exists) {
+        assert.isTrue(!err);
+        assert.isFalse(exists);
+      }
+    }
+  }
+}).export(module);
---
+++ test_black_edition/winston/test/transports/file2-test.js
@@ -0,0 +1,28 @@
+/*
+ * file-test.js: Tests for instances of the File transport
+ *
+ * (C) 2010 Charlie Robbins
+ * MIT LICENSE
+ *
+ */
+
+var path = require('path'),
+    vows = require('vows'),
+    fs = require('fs'),
+    assert = require('assert'),
+    winston = _winston,
+    helpers = require('../helpers');
+
+var stream = fs.createWriteStream(path.join(__dirname, '..', 'fixtures', 'logs', 'testfile.log')),
+    fileTransport = new (winston.transports.File)({ filename: path.join(__dirname, '..', 'fixtures', 'logs', 'testfilename.log') }),
+    streamTransport = new (winston.transports.File)({ stream: stream });
+
+vows.describe('winston/transports/file').addBatch({
+  "An instance of the File Transport": {
+    "when passed a valid filename": {
+      "should have the proper methods defined": function () {
+        //console.log(fileTransport instanceof winston.transports.File);
+      }
+    }
+  }
+}).export(module);
